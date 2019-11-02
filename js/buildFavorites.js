532cc287fad53d87c64e4fe19513afaa29d0df3d
Sufer_Qin <1105042987@qq.com>
2019-11-02 15:11:46 +0800

    Initial commit

---
+++ js/buildFavorites.js
@@ -0,0 +1,103 @@
+var openFile = function(event) {
+    var input = event.target;
+    var reader = new FileReader();
+    reader.onload = function() {
+        if(reader.result) {
+            //显示文件内容
+            json=eval(reader.result);
+            for(var i in json){
+                window.localStorage[json[i]['name']]=json[i]['url']
+            }
+            document.getElementById('mask').style.display = 'none'
+            refresh()
+        }
+    };
+    reader.readAsText(input.files[0]);
+};
+
+let judgeClick = function(name,url){
+    if (isDelete == 0){
+        window.open(url,'_self')
+    }
+    else{
+        window.localStorage.removeItem(name)
+        refresh()
+    }
+}
+
+var isDelete = 0;
+let switchDelete=function(){
+    if (document.getElementById('deletemask').style.display == 'none'){
+        document.getElementById('deletemask').style.display = '';
+        isDelete=1;
+    }
+    else{
+        document.getElementById('deletemask').style.display = 'none';
+        isDelete=0;
+    }
+}
+
+let createChild = function(fa,name,url){
+    let subbutton = document.createElement("button");
+    subbutton.setAttribute("class","ui button");
+    subbutton.setAttribute("onclick","judgeClick('"+name+"','"+url+"')");
+    subbutton.style.zIndex=100;
+    subbutton.style.position='relative';
+    subbutton.innerHTML=name;
+    fa.appendChild(subbutton);
+}
+
+let createAppend = function(fa){
+    let subbutton = document.createElement("button");
+    subbutton.setAttribute("class","ui button");
+    subbutton.setAttribute("onclick","init()");
+    subbutton.innerHTML='+';
+    fa.appendChild(subbutton);
+}
+
+let init = function(){
+    document.getElementById('mask').style.display=''
+    document.getElementById('names').value=''
+    document.getElementById('urls').value=''
+}
+
+let putObj = function() {
+    var name = document.getElementById('names').value;
+    var url = document.getElementById('urls').value;
+    window.localStorage[name]=url;
+    document.getElementById('mask').style.display='none';
+    refresh()
+}
+
+let refresh = function(){
+    var fa = document.getElementById("userFavorites");
+    fa.innerHTML=''
+    for (var i=0;i<window.localStorage.length;i++){
+        name = window.localStorage.key(i);
+        createChild(fa,name,localStorage.getItem(name))
+    }
+    createAppend(fa)
+}
+
+function Copy(){
+	const input = document.createElement('input');
+    input.setAttribute('readonly', 'readonly');
+    var str = '['
+    for (var i=0;i<window.localStorage.length;i++){
+        var name = window.localStorage.key(i);
+        var url = localStorage.getItem(name)
+        if (i!=0) str+=',';
+        str += "{\"name\":\""+name+"\",\"url\":\""+url+"\"}"
+    }
+    str+=']'
+    input.setAttribute('value',str);
+    document.body.appendChild(input);
+    input.setSelectionRange(0, 9999);
+    input.select()
+	if (document.execCommand('copy')) {
+		document.execCommand('copy');
+        // console.log(str);
+        alert('Copy Successed')
+	}
+    document.body.removeChild(input);
+}
---
+++ js/search.js
@@ -0,0 +1,67 @@
+const sInfor = document.getElementById("search");
+
+//isEmpty;
+let isEmpty =function (obj)  {
+    if (obj === null) return true;
+    if (typeof obj === 'undefined') {
+      return true;
+    }
+    if (typeof obj === 'string') {
+        let reg = new RegExp("^([ ]+)|([　]+)$");
+        //判断有空格，如果有空格，删除左边空格；
+        if( reg.test(obj)){
+            obj=obj.replace(/(^\s*)/g,"");
+        }
+
+      if (obj === "") {
+        return true;
+      }
+    }
+    return false;
+}
+//doAction 搜索跳转方法
+/*  
+p：搜索网址前段 
+n: 搜到网址后段
+h: 搜索首页
+*/
+let doAction=function(p,n,s){
+    if(isEmpty(sInfor.value)){
+		window.open(s, '_self');
+    }else{
+        window.open(p+sInfor.value+n,'_self');
+    }
+    
+}
+
+//拼接网址
+let p='';
+let n='';
+let s=''
+
+document.onkeydown = function (e) { // 回车提交表单
+    // 兼容FF和IE和Opera
+  var theEvent = window.event || e;
+	var code = theEvent.keyCode || theEvent.which || theEvent.charCode;
+	if (document.getElementById('mask').style.display=='none' && code == 13) {
+		if (window.event.ctrlKey == 1) {
+			p = "https://www.google.com/search?q=";
+			n = "&gws_rd=cr&nfpr=1&newwindow=1&num=30";
+			s = 'https://www.google.com/webhp?gws_rd=cr&nfpr=1&newwindow=1&num=30';
+			doAction(p, n, s);
+		}
+		else if (window.event.shiftKey == 1) {
+      var reg= /^[A-Za-z]+$/;
+      var lang = 'en'
+      if (reg.test(sInfor.value[0])){lang='zh-CN'}
+      p = 'https://translate.google.cn/#view=home&op=translate&sl=auto&tl='+lang+'&text=';
+			n = '';
+			s = 'https://translate.google.cn/';
+			doAction(p, n, s);
+		}
+		else{
+			p = "https://www.baidu.com/s?wd=";
+			doAction(p, '', p);
+		}
+	}
+}
